bad33086b2f93f802e1dee25ff1f307cf4b365b5
hungwei0331 <hungwei0331@gmail.com>
2023-12-20 00:33:57 +0800

    add 485. Max Consecutive Ones

--- Explore/Array101/485. Max Consecutive Ones/MyPlayground.playground/Contents.swift
+++ Explore/Array101/485. Max Consecutive Ones/MyPlayground.playground/Contents.swift
@@ -1,3 +1,32 @@
-import UIKit
+/*
+ Given a binary array nums, return the maximum number of consecutive 1's in the array.
 
-var greeting = "Hello, playground"
+ Example 1:
+ Input: nums = [1,1,0,1,1,1]
+ Output: 3
+ Explanation: The first two digits or the last three digits are consecutive 1s. The maximum number of consecutive 1s is 3.
+ 
+ Example 2:
+ Input: nums = [1,0,1,1,0,1]
+ Output: 2
+ */
+
+class Solution {
+    func findMaxConsecutiveOnes(_ nums: [Int]) -> Int {
+        var maxConsecutive = 0
+        var count = 0
+        
+        for num in nums {
+            if num == 1 {
+                count = count + 1
+            } else {
+                maxConsecutive = max(maxConsecutive, count)
+                count = 0
+            }
+        }
+        return max(maxConsecutive, count)
+    }
+}
+
+let result = Solution().findMaxConsecutiveOnes([1])
+print("result", result)
