6ad8ec2c5b5dbd2a5b1fdba5b9e17cc1beca836a
hungwei0331 <hungwei0331@gmail.com>
2024-01-03 22:13:21 +0800

    update 1. Two Sum

--- Problems/1. Two Sum /TwoSum.playground/Contents.swift
+++ Problems/1. Two Sum /TwoSum.playground/Contents.swift
@@ -3,10 +3,18 @@
 //You may assume that each input would have exactly one solution, and you may not use the same element twice.
 
 /*
-Example:
-Given nums = [2, 7, 11, 15], target = 9,
-Because nums[0] + nums[1] = 2 + 7 = 9,
-return [0, 1].
+ Example 1:
+ Input: nums = [2,7,11,15], target = 9
+ Output: [0,1]
+ Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
+ 
+ Example 2:
+ Input: nums = [3,2,4], target = 6
+ Output: [1,2]
+ 
+ Example 3:
+ Input: nums = [3,3], target = 6
+ Output: [0,1]
 */
 
 //let nums   = [2, 7, 11, 15]
@@ -15,30 +23,42 @@ return [0, 1].
 let nums   = [3,2,4]
 let target = 6
 
-// 90 ms, faster than 30.31% of Swift online submissions for Two Sum.
-// kind of bubble sort
-
-func twoSum(_ nums: [Int], _ target: Int) -> [Int] {
-    
-    var fristIndex: Int = 0
-    var secondIndex: Int = 0
-    
-    for (index, value) in nums.enumerated() {
+class Solution1 {
+    func twoSum(_ nums: [Int], _ target: Int) -> [Int] {
         
-        let nextIndex = index + 1
+        var fristIndex: Int = 0
+        var secondIndex: Int = 0
         
-        for nextIndex in nextIndex..<nums.count {
+        for (index, value) in nums.enumerated() {
             
-            if value + nums[nextIndex] == target {
-                fristIndex = index
-                secondIndex = nextIndex
+            let nextIndex = index + 1
+            
+            for nextIndex in nextIndex..<nums.count {
+                
+                if value + nums[nextIndex] == target {
+                    fristIndex = index
+                    secondIndex = nextIndex
+                }
             }
+            
         }
-        
+        return [fristIndex, secondIndex]
     }
-    return [fristIndex, secondIndex]
 }
 
-twoSum(nums, target)
+class Solution2 {
+    func twoSum(_ nums: [Int], _ target: Int) -> [Int] {
+        var dict = [Int: Int]()
+        for (index, value) in nums.enumerated() {
+            if let tmpIndex = dict[target - value] {
+                return [index, tmpIndex]
+            }
+            dict[value] = index
+        }
+        return [0]
+    }
+}
+
+let result = Solution2().twoSum([2,7,11,15], 9)
 
 
