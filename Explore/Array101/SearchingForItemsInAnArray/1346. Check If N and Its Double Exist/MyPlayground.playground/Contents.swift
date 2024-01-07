c99cee3258313c5a32871067fd305b52840feb94
hungwei0331 <hungwei0331@gmail.com>
2024-01-07 21:44:31 +0800

    add 1346. Check If N and Its Double Exist

--- Explore/Array101/SearchingForItemsInAnArray/1346. Check If N and Its Double Exist/MyPlayground.playground/Contents.swift
+++ Explore/Array101/SearchingForItemsInAnArray/1346. Check If N and Its Double Exist/MyPlayground.playground/Contents.swift
@@ -1,3 +1,38 @@
-import UIKit
+/*
+ Given an array arr of integers, check if there exist two indices i and j such that :
 
-var greeting = "Hello, playground"
+ i != j
+ 0 <= i, j < arr.length
+ arr[i] == 2 * arr[j]
+  
+
+ Example 1:
+ Input: arr = [10,2,5,3]
+ Output: true
+ Explanation: For i = 0 and j = 2, arr[i] == 10 == 2 * 5 == 2 * arr[j]
+ 
+ Example 2:
+ Input: arr = [3,1,7,11]
+ Output: false
+ Explanation: There is no i and j that satisfy the conditions.
+ */
+
+//https://www.youtube.com/watch?v=DkDcLtvrtNc
+
+class Solution {
+    func checkIfExist(_ arr: [Int]) -> Bool {
+        var seen = Set<Int>()
+        
+        for num in arr {
+            if seen.contains(num * 2) || seen.contains(num / 2) {
+                return true
+            }
+            seen.insert(num)
+        }
+        return false
+    }
+}
+
+let result = Solution().checkIfExist([-16,-13,8])
+//let result = Solution().checkIfExist([7,1,14,11])
+print("result", result)
