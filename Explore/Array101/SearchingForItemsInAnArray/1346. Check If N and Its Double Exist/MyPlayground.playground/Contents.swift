8147b24b2a81a67b31864de7499899d57b1c0685
hungwei0331 <hungwei0331@gmail.com>
2024-01-09 11:55:06 +0800

    update 1346. Check If N and Its Double Exist

--- Explore/Array101/SearchingForItemsInAnArray/1346. Check If N and Its Double Exist/MyPlayground.playground/Contents.swift
+++ Explore/Array101/SearchingForItemsInAnArray/1346. Check If N and Its Double Exist/MyPlayground.playground/Contents.swift
@@ -22,9 +22,9 @@
 class Solution {
     func checkIfExist(_ arr: [Int]) -> Bool {
         var seen = Set<Int>()
-        
         for num in arr {
-            if seen.contains(num * 2) || seen.contains(num / 2) {
+            if seen.contains( num * 2 ) { return true }
+            if num % 2 == 0 && seen.contains( num / 2 ) {
                 return true
             }
             seen.insert(num)
