5024962d45e89dfd2698d58fe2ee14f0a444c828
hungwei0331 <hungwei0331@gmail.com>
2024-05-08 23:21:09 +0800

    add 136. Single Number

---
+++ Explore/HashTable/PracticalApplication/136. Single Number/MyPlayground.playground/Contents.swift
@@ -0,0 +1,34 @@
+/*
+ Given a non-empty array of integers nums, every element appears twice except for one. Find that single one.
+
+ You must implement a solution with a linear runtime complexity and use only constant extra space.
+
+ Example 1:
+ Input: nums = [2,2,1]
+ Output: 1
+ 
+ Example 2:
+ Input: nums = [4,1,2,1,2]
+ Output: 4
+ 
+ Example 3:
+ Input: nums = [1]
+ Output: 1
+ */
+
+// TC: O(n) SC: O(n)
+class Solution {
+    func singleNumber(_ nums: [Int]) -> Int {
+        var dict = [Int: Int]()
+        for num in nums {
+            dict[num] = (dict[num] ?? 0) + 1
+        }
+        
+        for (key, value) in dict {
+            if value == 1 {
+                return key
+            }
+        }
+        return -1
+    }
+}
