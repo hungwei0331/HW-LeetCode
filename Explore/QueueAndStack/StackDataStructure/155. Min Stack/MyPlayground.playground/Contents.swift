5459484e405007ab647b3ecfbfefd07d7e820a96
hungwei0331 <hungwei0331@gmail.com>
2024-06-17 11:49:35 +0800

    add 155. Min Stack

---
+++ Explore/QueueAndStack/StackDataStructure/155. Min Stack/MyPlayground.playground/Contents.swift
@@ -0,0 +1,63 @@
+/*
+ Design a stack that supports push, pop, top, and retrieving the minimum element in constant time.
+
+ Implement the MinStack class:
+
+ MinStack() initializes the stack object.
+ void push(int val) pushes the element val onto the stack.
+ void pop() removes the element on the top of the stack.
+ int top() gets the top element of the stack.
+ int getMin() retrieves the minimum element in the stack.
+ You must implement a solution with O(1) time complexity for each function.
+
+ Example 1:
+ Input
+ ["MinStack","push","push","push","getMin","pop","top","getMin"]
+ [[],[-2],[0],[-3],[],[],[],[]]
+
+ Output
+ [null,null,null,null,-3,null,0,-2]
+
+ Explanation
+ MinStack minStack = new MinStack();
+ minStack.push(-2);
+ minStack.push(0);
+ minStack.push(-3);
+ minStack.getMin(); // return -3
+ minStack.pop();
+ minStack.top();    // return 0
+ minStack.getMin(); // return -2
+ */
+
+class MinStack {
+    
+    var stack: [Int]
+    var minStack: [Int]
+
+    init() {
+        stack = [Int]()
+        minStack = [Int]()
+    }
+    
+    func push(_ val: Int) {
+        stack.append(val)
+        if minStack.isEmpty || val < minStack.last! {
+            minStack.append(val)
+        }
+    }
+    
+    func pop() {
+        if let topElement = stack.popLast(), topElement == minStack.last {
+            minStack.popLast()
+        }
+        stack.popLast()
+    }
+    
+    func top() -> Int {
+        stack.last!
+    }
+    
+    func getMin() -> Int {
+        minStack.last!
+    }
+}
