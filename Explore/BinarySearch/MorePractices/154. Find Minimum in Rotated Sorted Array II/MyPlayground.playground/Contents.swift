a5933321302628d383373ab4b2e1369accd8faba
hungwei0331 <hungwei0331@gmail.com>
2024-10-08 23:41:25 +0800

    add 154. Find Minimum in Rotated Sorted Array II

---
+++ Explore/BinarySearch/MorePractices/154. Find Minimum in Rotated Sorted Array II/MyPlayground.playground/Contents.swift
@@ -0,0 +1,42 @@
+/*
+ Suppose an array of length n sorted in ascending order is rotated between 1 and n times. For example, the array nums = [0,1,4,4,5,6,7] might become:
+
+ [4,5,6,7,0,1,4] if it was rotated 4 times.
+ [0,1,4,4,5,6,7] if it was rotated 7 times.
+ Notice that rotating an array [a[0], a[1], a[2], ..., a[n-1]] 1 time results in the array [a[n-1], a[0], a[1], a[2], ..., a[n-2]].
+
+ Given the sorted rotated array nums that may contain duplicates, return the minimum element of this array.
+
+ You must decrease the overall operation steps as much as possible.
+
+ Example 1:
+ Input: nums = [1,3,5]
+ Output: 1
+ 
+ Example 2:
+ Input: nums = [2,2,2,0,1]
+ Output: 0
+ */
+
+// [2,2,2,2,2,0,1,2,2]
+// [2,2,3,4,2,2,2,2,2]
+// TC: O(N) SC: O(1)
+class Solution {
+    func findMin(_ nums: [Int]) -> Int {
+        var left = 0
+        var right = nums.count - 1
+        
+        while left < right {
+            let mid = left + (right - left) / 2
+            
+            if nums[mid] > nums[right] {
+                left = mid + 1
+            } else if nums[mid] < nums[right] {
+                right = mid
+            } else {
+                right = right - 1
+            }
+        }
+        return nums[left]
+    }
+}
