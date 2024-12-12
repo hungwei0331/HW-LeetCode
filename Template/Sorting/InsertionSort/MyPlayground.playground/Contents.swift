3510e6345752d86980fcf66e717e354fc2d5ab71
hungwei0331 <hungwei0331@gmail.com>
2024-12-12 10:42:07 +0800

    add Insertion Sort to template

---
+++ Template/Sorting/InsertionSort/MyPlayground.playground/Contents.swift
@@ -0,0 +1,21 @@
+
+// Insertion Sort
+// TC: O(n^2) SC: O(1)
+
+class Solution {
+    func insertionSort(_ list: inout [Int]) {
+        for i in 0..<list.count {
+            var currentIndex = i
+            while currentIndex > 0 && list[currentIndex] < list[currentIndex - 1] {
+                list.swapAt(currentIndex, currentIndex - 1)
+                currentIndex = currentIndex - 1
+            }
+        }
+    }
+}
+
+var list = [7, 3, 2, 5, 6, 10, 9, 8, 1]
+Solution().insertionSort(&list)
+print(list)
+print()
+
