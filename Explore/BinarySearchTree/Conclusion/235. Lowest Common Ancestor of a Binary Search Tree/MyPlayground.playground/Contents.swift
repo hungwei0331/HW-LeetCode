63e4645e96724bccfea526e5c6b018d4d427f3ea
hungwei0331 <hungwei0331@gmail.com>
2025-03-02 22:05:43 +0800

    add 235. Lowest Common Ancestor of a Binary Search Tree

---
+++ Explore/BinarySearchTree/Conclusion/235. Lowest Common Ancestor of a Binary Search Tree/MyPlayground.playground/Contents.swift
@@ -0,0 +1,61 @@
+/*
+ Given a binary search tree (BST), find the lowest common ancestor (LCA) node of two given nodes in the BST.
+
+ According to the definition of LCA on Wikipedia: “The lowest common ancestor is defined between two nodes p and q as the lowest node in T that has both p and q as descendants (where we allow a node to be a descendant of itself).”
+
+ Example 1:
+ Input: root = [6,2,8,0,4,7,9,null,null,3,5], p = 2, q = 8
+ Output: 6
+ Explanation: The LCA of nodes 2 and 8 is 6.
+ 
+ Example 2:
+ Input: root = [6,2,8,0,4,7,9,null,null,3,5], p = 2, q = 4
+ Output: 2
+ Explanation: The LCA of nodes 2 and 4 is 2, since a node can be a descendant of itself according to the LCA definition.
+ 
+ Example 3:
+ Input: root = [2,1], p = 2, q = 1
+ Output: 2
+  
+
+ Constraints:
+ The number of nodes in the tree is in the range [2, 105].
+ -109 <= Node.val <= 109
+ All Node.val are unique.
+ p != q
+ p and q will exist in the BST.
+ */
+
+public class TreeNode {
+    public var val: Int
+    public var left: TreeNode?
+    public var right: TreeNode?
+    public init(_ val: Int) {
+        self.val = val
+        self.left = nil
+        self.right = nil
+    }
+}
+
+// TC: O(H), SC: O(1)
+class Solution {
+   func lowestCommonAncestor(_ root: TreeNode?, _ p: TreeNode?, _ q: TreeNode?) -> TreeNode? {
+       guard
+           let p = p,
+           let q = q
+       else { return nil }
+       
+       var current = root
+       
+       while current != nil {
+           if p.val < current!.val && q.val < current!.val {
+               current = current?.left
+           } else if p.val > current!.val && q.val > current!.val {
+               current = current?.right
+           } else {
+               return current
+           }
+       }
+       return nil
+   }
+}
