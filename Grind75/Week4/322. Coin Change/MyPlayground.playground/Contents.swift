40fe32e62137314349e52f888ddec525918e4a01
hungwei0331 <hungwei0331@gmail.com>
2025-06-06 21:16:13 +0800

    update 322. Coin change answer

--- Grind75/Week4/322. Coin Change/MyPlayground.playground/Contents.swift
+++ Grind75/Week4/322. Coin Change/MyPlayground.playground/Contents.swift
@@ -35,16 +35,15 @@
 class Solution {
     func coinChange(_ coins: [Int], _ amount: Int) -> Int {
         if amount == 0 { return 0 }
-        
+
         var dp = Array(repeating: amount + 1, count: amount + 1)
         dp[0] = 0
-        
-        for i in 0..<amount {
+
+        for i in 0...amount {
             for coin in coins {
                 if i - coin >= 0 {
                     dp[i] = min(dp[i], dp[i - coin] + 1)
                 }
-                
             }
         }
         return dp[amount] > amount ? -1 : dp[amount]
