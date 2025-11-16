c1ed0d79ce3d7eb5513ccf2e7421141adc58ffe5
hungwei0331 <hungwei0331@gmail.com>
2025-11-16 23:45:36 +0800

    add 8. String to Integer (atoi)

---
+++ Grind75/Week6/8. String to Integer (atoi)/MyPlayground.playground/Contents.swift
@@ -0,0 +1,51 @@
+/*
+ Implement the myAtoi(string s) function, which converts a string to a 32-bit signed integer.
+
+ The algorithm for myAtoi(string s) is as follows:
+
+ Whitespace: Ignore any leading whitespace (" ").
+ Signedness: Determine the sign by checking if the next character is '-' or '+', assuming positivity if neither present.
+ Conversion: Read the integer by skipping leading zeros until a non-digit character is encountered or the end of the string is reached. If no digits were read, then the result is 0.
+ Rounding: If the integer is out of the 32-bit signed integer range [-231, 231 - 1], then round the integer to remain in the range. Specifically, integers less than -231 should be rounded to -231, and integers greater than 231 - 1 should be rounded to 231 - 1.
+ Return the integer as the final result.
+
+ */
+
+class Solution {
+    func myAtoi(_ s: String) -> Int {
+        var chars = Array(s)
+        var i = 0
+        
+        while i < chars.count && chars[i] == " " {
+            i = i + 1
+        }
+        
+        if i == chars.count { return 0 }
+        
+        var sign = 1
+        if chars[i] == "-" {
+            sign = -1
+            i = i + 1
+        } else if chars[i] == "+" {
+            i = i + 1
+        }
+        
+        var number = 0
+        var maxValue = Int(Int32.max) // 2147483647
+        var minValue = Int(Int32.min) // -2147483648
+        
+        while i < chars.count {
+            let char = chars[i]
+            
+            guard let digital = char.wholeNumberValue else { break }
+            
+            if number > maxValue / 10 || (number == maxValue / 10 && digital > maxValue % 10) {
+                return sign == 1 ? maxValue : minValue
+            }
+            
+            number = number * 10 + digital
+            i = i + 1
+        }
+        return number * sign
+    }
+}
